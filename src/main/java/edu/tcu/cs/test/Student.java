0f580f4696e1afaef54c372ae1e5a7b24f210bfb
hienkyle <hien2003.dm@gmail.com>
2024-03-23 22:28:50 -0500

    add address

--- src/main/java/edu/tcu/cs/test/Student.java
+++ src/main/java/edu/tcu/cs/test/Student.java
@@ -3,11 +3,21 @@ package edu.tcu.cs.test;
 public class Student {
     private String name;
 
+    private String address;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
